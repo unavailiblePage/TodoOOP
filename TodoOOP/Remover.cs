20c754b519c0bcc0d54093f0615ed971908aa250
MIB <jajan151@seznam.cz>
2023-12-20 19:41:43 +0100

    Remove function added

---
+++ TodoOOP/Remover.cs
@@ -0,0 +1,31 @@
+﻿namespace TodoOOP;
+
+public class Remover
+{
+    public void Remove(List<Todo> Todos, Writer ConsoleOutput, Reader ConsoleInput, string fullPath)
+    {
+        ConsoleOutput.Write(Todos);
+        Console.WriteLine();
+
+        int lineToRemove = ConsoleInput.ReadTodoIndex();
+        
+        Todos.RemoveAt(lineToRemove - 1);
+        
+        Console.Clear();
+        
+        ConsoleOutput.Write(Todos);
+        Console.WriteLine();
+        
+        Console.WriteLine("The todo has been removed. Press any key to continue...");
+        Console.ReadKey();
+
+        ConsoleOutput.WriteToFile(Todos, fullPath);
+
+        Console.Clear();
+
+        ConsoleOutput.Write(Todos);
+        Console.WriteLine();
+
+        Console.WriteLine("The todo has been removed. Press any key to continue...");
+    }
+}
